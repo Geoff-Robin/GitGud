2389d3bd31e614804693d61a85264ddc4b6efbed
Geoff-Robin <jeffrobin132004@gmail.com>
2025-03-12 01:13:54 +0530

    updated server.js

--- code_runner/server.js
+++ code_runner/server.js
@@ -1,52 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { exec } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 app.use(bodyParser.json());
 
-const tempDir = path.join(__dirname, 'temp');
+const tempDir = path.join(__dirname, "temp");
 if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir);
+  fs.mkdirSync(tempDir);
 }
 
-app.post('/execute', (req, res) => {
-    const { language, code } = req.body;
-    let command;
-    let filename, executable;
+app.post("/execute", (req, res) => {
+  const { language, code } = req.body;
+  let command;
+  let filename, executable;
 
-    switch (language) {
-        case 'python':
-            filename = path.join(tempDir, 'temp.py');
-            fs.writeFileSync(filename, code);
-            command = `python3 ${filename}`;
-            break;
-        case 'c':
-        case 'c++':
-            const extension = language === 'c' ? 'c' : 'cpp';
-            filename = path.join(tempDir, `temp.${extension}`);
-            executable = path.join(tempDir, 'temp_executable');
-            fs.writeFileSync(filename, code);
-            command = `g++ ${filename} -o ${executable} && ${executable}`;
-            break;
-        default:
-            return res.status(400).send('Unsupported language');
+  switch (language) {
+    case "python":
+      filename = path.join(tempDir, "temp.py");
+      fs.writeFileSync(filename, code);
+      command = `python3 ${filename}`;
+      break;
+    case "c":
+    case "c++":
+      const extension = language === "c" ? "c" : "cpp";
+      filename = path.join(tempDir, `temp.${extension}`);
+      executable = path.join(tempDir, "temp_executable");
+      fs.writeFileSync(filename, code);
+      command = `g++ ${filename} -o ${executable} && ${executable}`;
+      break;
+    default:
+      return res.status(400).send({
+        error: "Unsupported language",
+      });
+  }
+
+  exec(command, (error, stdout, stderr) => {
+    fs.unlinkSync(filename);
+    if (executable && fs.existsSync(executable)) fs.unlinkSync(executable);
+
+    if (error) {
+      return res.status(500).send(stderr);
     }
-
-    exec(command, (error, stdout, stderr) => {
-        fs.unlinkSync(filename);
-        if (executable && fs.existsSync(executable)) fs.unlinkSync(executable);
-
-        if (error) {
-            return res.status(500).send(stderr);
-        }
-        res.send(stdout);
+    res.status(200).send({
+        output: stdout
     });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
