a08b6c59e8618257292361bc9d60b6067cbf7da1
Geoff-Robin <jeffrobin132004@gmail.com>
2025-03-22 20:53:20 +0530

    updated auth_utils.py

--- backend/auth_utils.py
+++ backend/auth_utils.py
@@ -1,9 +1,13 @@
 import os
 from passlib.context import CryptContext
 from dotenv import load_dotenv
-from typing import Union, Any
+from typing import Union, Any, Annotated
 from jose import jwt
+from fastapi import Depends, HTTPException, status, Request
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 import datetime
+from jwt.exceptions import InvalidTokenError
 
 load_dotenv()
 
@@ -13,9 +17,21 @@ REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24
 ALGORITHM = "HS256"
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 JWT_REFRESH_SECRET_KEY = os.getenv("JWT_REFRESH_SECRET_KEY")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-async def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+async def get_user(db, email: str):
+    user = db.find_one({"email": email})
+    return user
+
+
+async def create_access_token(
+    subject: Union[str, Any], expires_delta: int = None
+) -> str:
     if expires_delta is not None:
         expires_delta = datetime.datetime.now(datetime.timezone.utc) + expires_delta
     else:
@@ -27,7 +43,9 @@ async def create_access_token(subject: Union[str, Any], expires_delta: int = Non
     return encoded_jwt
 
 
-async def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+async def create_refresh_token(
+    subject: Union[str, Any], expires_delta: int = None
+) -> str:
     if expires_delta is not None:
         expires_delta = datetime.datetime.now(datetime.timezone.utc) + expires_delta
     else:
@@ -46,3 +64,25 @@ async def verify_password(password: str, hashed_pass: str) -> bool:
 
 async def get_hashed_password(password: str) -> str:
     return password_context.hash(password)
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], request: Request
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(request.app.database, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
