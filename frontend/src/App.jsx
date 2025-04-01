5f2be4616fc68ad4797b7613b655774fe2c78ef8
Geoff-Robin <jeffrobin132004@gmail.com>
2025-04-01 17:28:35 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,21 @@ import { ThemeProvider } from "./components/ui/themecotext";
 import Dashboard from "./components/Dashboard";
 import SignUpPage from "./components/SignUpPage";
 import LoginPage from "./components/LoginPage";
+import { AuthContextProvider } from "./context/auth-context";
 
 function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <AuthContextProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthContextProvider>
   );
 }
 
